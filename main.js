19302fbafd9c953f5615cc17765e1762e368809b
leomaltta <leonardoomaltaa2@gmail.com>
2023-08-24 22:32:38 -0300

    updating

--- main.js
+++ main.js
@@ -1,29 +1,27 @@
-async function searchAdress(cep) {
-    const msgErro = document.querySelector('#erro');
-    msgErro.innerHTML = "";
-    try {
+async function searchAddress(cep) {
+  const msgErro = document.querySelector("#erro");
+  msgErro.innerHTML = "";
+  try {
     var consultaCEP = await fetch(`https://viacep.com.br/ws/${cep}/json/`);
     var consultaCEPConverted = await consultaCEP.json();
     if (consultaCEPConverted.erro) {
-        throw Error('CEP inexistente');
+      throw Error("CEP inexistente");
     }
-    const cidade = document.querySelector('#cidade');
-    const logradouro = document.querySelector('#endereco')
-    const estado = document.querySelector('#estado')
-    
+    const cidade = document.querySelector("#cidade");
+    const logradouro = document.querySelector("#endereco");
+    const estado = document.querySelector("#estado");
+
     cidade.value = consultaCEPConverted.localidade;
     logradouro.value = consultaCEPConverted.logradouro;
     estado.value = consultaCEPConverted.uf;
-    
+
     console.log(consultaCEPConverted);
     return consultaCEPConverted;
-
-    } catch (erro) {
-        msgErro.innerHTML = `<p>CEP Inválido. Tente novamente!</p>`
-        console.log(erro);
-    }
-
+  } catch (erro) {
+    msgErro.innerHTML = `<p>CEP Inválido. Tente novamente!</p>`;
+    console.log(erro);
+  }
 }
 
-const cep = document.querySelector('#cep');
-cep.addEventListener('focusout', () => searchAdress(cep.value))
+const cep = document.querySelector("#cep");
+cep.addEventListener("focusout", () => searchAddress(cep.value));
